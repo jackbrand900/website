a54bfeb07dc43bb32882de4b2fe04fd7fa256e16
Jack Brand <jackbrand900@gmail.com>
2025-02-22 19:45:17 +0000

    testing

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, blank=True, null=True)
     published_date = models.DateField()
 
     class Meta:
